b184cd4fdc1f750598a49a6e93a3b8ab9ba551a7
Jacob Colvin <jacobcolvin1@gmail.com>
2025-07-06 19:28:32 -0400

    docs: add docs for json schemas

--- pkg/config/doc.go
+++ pkg/config/doc.go
@@ -2,4 +2,7 @@
 //
 // It wraps other package configuration to provide a single API for
 // loading, validating, and writing configuration files in YAML format.
+//
+// Additionally, it implements JSON schema generation and validation for the
+// configuration.
 package config
